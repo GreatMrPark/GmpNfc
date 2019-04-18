125032c334e0087fef473ebfac30c8887ec7314f
Great Mr Park <greatmrpark@gmail.com>
2019-04-18 22:04:20 +0900

    Initial commit

---
+++ app/src/main/java/com/greatmrpark/gmpnfc/SenderSecondActivity.java
@@ -0,0 +1,13 @@
+package com.greatmrpark.gmpnfc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SenderSecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sender_second);
+    }
+}
